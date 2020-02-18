58cb21dbd6690759980f018b19edbcfb8950bb81
AMirshekari <amirshekari@ebates.com>
2020-02-17 16:26:52 -0800

    Generic Update

--- server/src/client/components/UsersList.js
+++ server/src/client/components/UsersList.js
@@ -9,7 +9,7 @@ class UsersList extends Component {
     }
 
     renderUsers() {
-        return this.props.users.map(user => <li key={user.id}>${user.name}</li>);
+        return this.props.users.map(user => <li key={user.id}>{user.name}</li>);
     }
 
     render() {
